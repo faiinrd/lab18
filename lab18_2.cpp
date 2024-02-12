ddca5f5b0ef771d6a98fd7b588acfc6e7aeb9ee8
faiinrd <nopradakrit@gmail.com>
2024-02-12 22:46:43 +0700

    lab18 completed

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,16 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *R1,Rect *R2){
+	double a,b,c,d;
+	a = min(((*R1).x)+(*R1).w,((*R2).x)+(*R2).w);
+	b = max((*R1).x,(*R2).x);
+	c = min((*R1).y,(*R2).y);
+	d = max(((*R1).y)-(*R1).h,((*R2).y)-(*R2).h);
+	double A = (a-b)*(c-d);
+	if(A>0){
+		return A;
+	}else{
+		return 0;
+	}
 }
